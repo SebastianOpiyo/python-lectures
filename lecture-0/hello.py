1afb6ea5dd284d2ce510afdb36a4ee03df8b3502
Sebastian A. Opiyo <saopiyo@usiu.ac.ke>
2023-01-27 17:24:40 +0300

    WIP:Introduction to python

---
+++ lecture-0/hello.py
@@ -0,0 +1,24 @@
+# print("Hello, world!")
+
+# CammelCase
+# cpascalCase
+# snake_case
+
+NAME=""
+NUMBERS=0
+
+def another_hello_world(string:str):
+    argument_passed = string
+    NUMBERS = 20
+    print(argument_passed, NUMBERS)
+
+
+
+another_hello_world("Function called")
+
+# variables
+
+
+
+# if __name__ == "__main__":
+#     another_hello_world("Print me")
